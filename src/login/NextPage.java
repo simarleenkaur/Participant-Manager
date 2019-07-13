4c529d915070411c6f8da390ec9efdad19c0bbec
simarleenkaur <simar07kaur1998@gmail.com>
2019-07-13 15:11:22 +0530

    first commit

---
+++ src/login/NextPage.java
@@ -0,0 +1,16 @@
+package login;
+
+//importing various packages or classes.
+import javax.swing.*;
+import java.awt.*;
+
+class NextPage extends JFrame { 
+	NextPage(){
+		//defining the default close operation that is closing using the top right button
+		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+		//setting the default button
+		setTitle("Welcome");
+		//setting the size of the window
+		setSize(1000,1000);
+	}
+}
